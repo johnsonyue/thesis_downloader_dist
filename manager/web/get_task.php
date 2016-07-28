50b58cdf91bc4284162c63e4b2cbe92458ab2d04
john <johnsonyue@163.com>
2016-07-29 02:48:17 +0800

    added config file

--- manager/web/get_task.php
+++ manager/web/get_task.php
@@ -1,6 +1,7 @@
 <?php
+	$code_path = parse_init_file("config.ini");
 	$node_id = $_POST["node_id"];
 	$node_key = $_POST["node_key"];
 	
-	echo exec ("python ../run.py auth "+$node_id+" "+$node_key);
+	echo exec ("python ".$code_path."/run.py auth ".$node_id." ".$node_key);
 ?>
