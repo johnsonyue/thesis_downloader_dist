4b050f3fa1a32281f6b6c70c909bc3dbe0a8e1a0
johnsonyue <johnsonyuehit@163.com>
2016-07-29 10:48:19 +0000

    added php config

--- manager/web/get_task.php
+++ manager/web/get_task.php
@@ -1,8 +1,14 @@
 <?php
-	$code_path = parse_init_file("config.ini");
-	$node_id = $_POST["node_id"];
-	$node_key = $_POST["node_key"];
+	$config = parse_ini_file("config.ini");
+	$code_path = $config["code_path"];
+
+	$node_id = $_POST["id"];
+	$node_key = $_POST["key"];
 	
-	echo "shit";
-	#echo exec ("python ".$code_path."/run.py auth ".$node_id." ".$node_key);
+	if (exec("python $code_path/run.py auth ".$node_id." ".$node_key) == "True"){
+		echo exec("python $code_path/run.py get_task");
+	}
+	else{
+		echo "auth failed";
+	}
 ?>
---
+++ manager/web/notify.php
@@ -0,0 +1,22 @@
+<?php
+	$config = parse_ini_file("config.ini");
+	$code_path = $config["code_path"];
+
+	$node_id = $_POST["id"];
+	$node_key = $_POST["key"];
+	$type = $_POST["type"];
+	$task = $_POST["task"];
+	
+	if (exec("python $code_path/run.py auth $node_id $node_key") == "True"){
+		if ($type != "finished"){
+			echo exec("python $code_path/run.py on_notify $type $node_id $task");
+		}
+		else{
+			$time_used = $_POST["time_used"];
+			echo exec("python $code_path/run.py on_notify $type $node_id $task $time_used");
+		}
+	}
+	else{
+		echo "auth failed";
+	}
+?>
