095e0ee8dc6d8068d1abcf44547d8f97ebb8c294
johnsonyue <johnsonyuehit@163.com>
2016-08-03 13:11:29 +0000

    small fixes

--- manager/config.py
+++ manager/config.py
@@ -1,2 +1,9 @@
-[code]
-code_path = /production/manager/
+import ConfigParser
+
+def get_config_section_dict(config_file, header):
+	parser = ConfigParser.ConfigParser();
+	parser.read(config_file);
+	config = {};
+	map( lambda x:config.setdefault(x[0], x[1]), parser.items(header) );
+
+	return config;
